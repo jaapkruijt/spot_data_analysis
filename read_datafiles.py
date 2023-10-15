39fd382fc235fbc51339d94bf7ae2a7a35b48967
Jaap <j.m.kruijt@vu.nl>
2023-10-15 11:43:34 +0200

    add more analyses and visualizations

--- read_datafiles.py
+++ read_datafiles.py
@@ -1,3 +1,4 @@
+import statistics
 from collections import Counter
 import os
 from sentence_transformers import SentenceTransformer, util
@@ -8,6 +9,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 model = SentenceTransformer('distiluse-base-multilingual-cased-v1')
+nlp = spacy.load('nl_core_news_sm')
 
 
 def prepare_tsv_datafile(path):
@@ -20,7 +22,11 @@ def prepare_tsv_datafile(path):
         df['character'] = df['character'].astype(str)
     except ValueError:
         df['character'] = df['character'].astype(str)
-    df['transaction_unit'] = df['transaction_unit'].astype(str)
+    try:
+        df['transaction_unit'] = df['transaction_unit'].astype(int)
+        df['transaction_unit'] = df['transaction_unit'].astype(str)
+    except ValueError:
+        df['transaction_unit'] = df['transaction_unit'].astype(str)
     df['round'] = df['round'].astype(str)
 
     return df
@@ -29,13 +35,11 @@ def prepare_tsv_datafile(path):
 def organize_mentions(datafile, experiment):
     mention_dict_per_tu = {}
     mention_dict_per_round = {}
+    transaction_units = {}
 
-    if experiment == 'AK':
-        for i in range(16):
-            mention_dict_per_tu[str(i)] = {}
-    elif experiment == 'EN':
-        for i in range(17):
-            mention_dict_per_tu[str(i)] = {}
+    char_amount = 15 if experiment == 'AK' else 16
+    for i in range(char_amount+1):
+        mention_dict_per_tu[str(i)] = {}
 
     for ind in datafile.index:
         if datafile['speaker'][ind] == 'robot' or datafile['round'][ind] == '0':
@@ -53,6 +57,11 @@ def organize_mentions(datafile, experiment):
                         tu = str(separate_tu[0])
                     else:
                         tu = separate_tu[i]
+                    # if tu in transaction_units.keys():
+                    #     transaction_units[tu]['characters'].append(character)
+                    # else:
+                    #     da_count = Counter()
+                    #     transaction_units[tu] = {'characters': [character], 'length': 0, 'repair_amt': 0, 'da': da_count}
                     mention_tuple = (mention, tu_relation)
                     if game_round in mention_dict_per_tu[character].keys():
                         if tu in mention_dict_per_tu[character][game_round]:
@@ -61,6 +70,17 @@ def organize_mentions(datafile, experiment):
                             mention_dict_per_tu[character][game_round][tu] = [mention_tuple]
                     else:
                         mention_dict_per_tu[character][game_round] = {tu: [mention_tuple]}
+    #
+    # for ind in datafile.index:
+    #     if datafile['transaction_unit'][ind] != '0' and datafile['round'][ind] != '0':
+    #         separate_tu = datafile['transaction_unit'][ind].split(';')
+    #         tu_relation = datafile['tu_relation'][ind]
+    #         for tu in separate_tu:
+    #             transaction_units[tu]['length'] += 1
+    #             dialog_act = datafile['da'][ind]
+    #             if tu_relation in ['req-repair', 'clar-repair']:
+    #                 transaction_units[tu]['repair_amt'] += 1
+    #             transaction_units[tu]['da'][dialog_act] += 1
 
     for character, game_rounds in mention_dict_per_tu.items():
         for game_round, units in game_rounds.items():
@@ -85,52 +105,157 @@ def organize_mentions(datafile, experiment):
     return mention_dict_per_tu, mention_dict_per_round
 
 
+def organize_transaction_units(datafile):
+    transaction_units = {}
+
+    for ind in datafile.index:
+        if datafile['mention'][ind] != 0:
+            separate_mentions = datafile['mention'][ind].split(';')
+            separate_characters = datafile['character'][ind].split(';')
+            separate_tu = datafile['transaction_unit'][ind].split(';')
+            # game_round = datafile['round'][ind]
+            # tu_relation = datafile['tu_relation'][ind]
+            for i, mention in enumerate(separate_mentions):
+                character = str(separate_characters[i])
+                if len(separate_tu) != len(separate_mentions):
+                    tu = str(separate_tu[0])
+                else:
+                    tu = separate_tu[i]
+                if tu in transaction_units.keys():
+                    transaction_units[tu]['characters'].append(character)
+                else:
+                    da_count = Counter()
+                    transaction_units[tu] = {'characters': [character], 'length': 0, 'repair_amt': 0, 'da': da_count}
+                # mention_tuple = (mention, tu_relation)
+                # if game_round in mention_dict_per_tu[character].keys():
+                #     if tu in mention_dict_per_tu[character][game_round]:
+                #         mention_dict_per_tu[character][game_round][tu].append(mention_tuple)
+                #     else:
+                #         mention_dict_per_tu[character][game_round][tu] = [mention_tuple]
+                # else:
+                #     mention_dict_per_tu[character][game_round] = {tu: [mention_tuple]}
+
+    for ind in datafile.index:
+        if datafile['transaction_unit'][ind] != '0':
+            separate_tu = datafile['transaction_unit'][ind].split(';')
+            tu_relation = datafile['tu_relation'][ind]
+            for tu in separate_tu:
+                transaction_units[tu]['length'] += 1
+                dialog_act = datafile['da'][ind]
+                if tu_relation in ['req-repair', 'clar-repair']:
+                    transaction_units[tu]['repair_amt'] += 1
+                transaction_units[tu]['da'][dialog_act] += 1
+
+    return transaction_units
+
+
 def participant_linguistic_analysis(experiment):
     directory = '/Users/jaapkruijt/Documents/ALANI/SPOT/SPOT_pilotadata/SPOT-mentions'
     filenames = [f for f in os.listdir(directory) if not f.startswith('.')]
-    character_mentions = {}
+    mc_mentions = {}
+    sc_mentions = {}
     char_amount = 15 if experiment == 'AK' else 13
+    round_amounts = ['1','2','3','4','5','6']
     character_mean_sim = {'character': [], 'round': [], 'sim_score': []}
     for i in ['1','2','3']:
-        character_mentions[i] = {}
-        for j in ['1','2','3','4','5','6']:
-            character_mentions[i][j] = []
+        mc_mentions[i] = {}
+        for j in round_amounts:
+            mc_mentions[i][j] = []
+    for i in range(4, char_amount+1):
+        sc_mentions[str(i)] = {}
+        for j in round_amounts:
+            sc_mentions[str(i)][str(j)] = []
     for filename in filenames:
+        exp = os.path.splitext(filename)[0].split('_')[0]
+        if exp != experiment:
+            continue
         f = os.path.join(directory, filename)
         data = prepare_tsv_datafile(f)
         mentions_per_tu, mentions_per_round = organize_mentions(data, experiment)
         for character, rounds in mentions_per_round.items():
-            if character not in ['1', '2', '3']:
+            if character == '0':
                 continue
-            for game_round, mentions in rounds.items():
-                character_mentions[character][game_round].extend(mentions)
-    for character, rounds in character_mentions.items():
+            if character not in ['1', '2', '3']:
+                for game_round, mentions in rounds.items():
+                    if game_round == '0':
+                        continue
+                    sc_mentions[character][game_round].extend(mentions)
+            else:
+                for game_round, mentions in rounds.items():
+                    if game_round == '0':
+                        continue
+                    mc_mentions[character][game_round].extend(mentions)
+    inner_mentions_count = Counter()
+    outer_mentions_count = Counter()
+    mentions_round_6 = {'inner': inner_mentions_count, 'outer': outer_mentions_count}
+    for character, rounds in mc_mentions.items():
         for game_round, mentions in rounds.items():
+            for mention in mentions:
+                doc = nlp(mention)
+                root = [token for token in doc if token.head == token][0]
+                mentions_round_6['inner'][root.pos_] += 1
             mention_embeddings = model.encode(mentions)
             sim_scores = util.cos_sim(mention_embeddings, mention_embeddings)
             mean = torch.mean(sim_scores)
             character_mean_sim['character'].append(character)
             character_mean_sim['round'].append(game_round)
             character_mean_sim['sim_score'].append(float(mean))
-
+    for character, rounds in sc_mentions.items():
+        for game_round, mentions in rounds.items():
+            for mention in mentions:
+                doc = nlp(mention)
+                root = [token for token in doc if token.head == token][0]
+                mentions_round_6['outer'][root.pos_] += 1
     dataframe = pd.DataFrame.from_dict(character_mean_sim)
-    return dataframe
+    return dataframe, mc_mentions, mentions_round_6
 
 
-# def character_discourse_analysis()
+def dataset_details(experiment, directory):
+    details = {'utterances': 0, 'mentions': 0, 'turns': 1, 'tu': [], 'repair': 0, 'da': Counter(), 'avg_length': []}
+    filenames = [f for f in os.listdir(directory) if not f.startswith('.')]
+    for filename in filenames:
+        exp = os.path.splitext(filename)[0].split('_')[0]
+        if exp != experiment:
+            continue
+        f = os.path.join(directory, filename)
+        dataset = prepare_tsv_datafile(f)
+        for ind in dataset.index:
+            if ind > 0:
+                if dataset['speaker'][ind] != dataset['speaker'][ind-1]:
+                    details['turns'] += 1
+            details['utterances'] += 1
+            if dataset['mention'][ind] != 0:
+                details['mentions'] += 1
+            if dataset['transaction_unit'][ind] != '0':
+                if dataset['tu_relation'][ind] in ['clar-repair', 'req-repair', 'correction']:
+                    details['repair'] += 1
+            dialog_act = dataset['da'][ind]
+            details['da'][dialog_act] += 1
+            transaction_units = organize_transaction_units(dataset)
+            units = sorted([int(key) for key in transaction_units.keys()])[-1]
+            details['tu'].append(units)
+            for tu, tu_details in transaction_units.items():
+                details['avg_length'].append(tu_details['length'])
+
+    return details
+
+
+def character_tu_analysis(characters):
+    character_tu_data = {}
+    for character, game_rounds in characters.items():
+        character_tu_data[character] = {}
+        for game_round, tus in game_rounds.items():
+            character_tu_data[character][game_round] = {'tu_amt': len(tus)}
+
+    return character_tu_data
 
 
 def character_linguistic_analysis(characters):
-    nlp = spacy.load('nl_core_news_sm')
     character_lemma_counts = {}
-    # character_desc_length = {}
-    # character_similarity_scores = {}
     character_data = {}
 
     for character, game_rounds in characters.items():
         lemma_count = Counter()
-        # character_desc_length[character] = {}
-        # character_similarity_scores[character] = {}
         character_data[character] = {}
         for game_round, mentions in game_rounds.items():
             desc_lengths = []
@@ -162,9 +287,9 @@ def character_linguistic_analysis(characters):
     return character_lemma_counts, character_data
 
 
-def combine_character_data(participant_number, experiment, character_data, combined_data):
+def combine_character_data(participant_number, experiment, character_data, tu_data, combined_data):
     for character, rounds in character_data.items():
-        for round, values in rounds.items():
+        for game_round, values in rounds.items():
             if not values:
                 continue
             else:
@@ -175,7 +300,7 @@ def combine_character_data(participant_number, experiment, character_data, combi
                     combined_data['experiment'].append('2')
                     combined_data['character'].append(f'2.{character}')
                 combined_data['participant'].append(participant_number)
-                combined_data['round'].append(round)
+                combined_data['round'].append(game_round)
                 if character in ['1','2','3']:
                     combined_data['circle'].append('inner')
                 else:
@@ -186,35 +311,87 @@ def combine_character_data(participant_number, experiment, character_data, combi
                     combined_data['sim_score'].append(values['sim_score'])
                 else:
                     combined_data['sim_score'].append(None)
+    for character, rounds in tu_data.items():
+        for game_round, values in rounds.items():
+            if not values:
+                continue
+            else:
+                combined_data['num_tus'].append(values['tu_amt'])
 
 
 def combine_experiments():
     directory = '/Users/jaapkruijt/Documents/ALANI/SPOT/SPOT_pilotadata/SPOT-mentions'
-    data = {'experiment': [], 'participant': [], 'character': [], 'round': [], 'circle': [], 'utt_len': [],
-                    'sim_score': [], 'num_mentions': []}
+    character_data = {'experiment': [], 'participant': [], 'character': [], 'round': [], 'circle': [], 'utt_len': [],
+                    'sim_score': [], 'num_mentions': [], 'num_tus': []}
+    transaction_data = {'experiment': [], 'participant': [], 'transaction_unit': [], 'length': [],
+                        'repair_relations': [], 'dialog act': []}
     filenames = [f for f in os.listdir(directory) if not f.startswith('.')]
     for filename in filenames:
         f = os.path.join(directory, filename)
         experiment = os.path.splitext(filename)[0].split('_')[0]
         participant = os.path.splitext(filename)[0].split('_')[1]
         mentions = prepare_tsv_datafile(f)
-        mentions_per_tu, mentions_per_round = organize_mentions(mentions, experiment)
-        word_counts, character_data = character_linguistic_analysis(mentions_per_round)
-        combine_character_data(participant, experiment, character_data, data)
+        mentions_per_tu, mentions_per_round= organize_mentions(mentions, experiment)
+        word_counts, mention_data = character_linguistic_analysis(mentions_per_round)
+        tu_data = character_tu_analysis(mentions_per_tu)
+        combine_character_data(participant, experiment, mention_data, tu_data, character_data)
 
-    dataframe = pd.DataFrame.from_dict(data)
+    dataframe = pd.DataFrame.from_dict(character_data)
     return dataframe
 
 
+def show_dataset_details():
+    en_dataset_details = dataset_details('EN', '/Users/jaapkruijt/Documents/ALANI/SPOT/SPOT_pilotadata/SPOT-mentions')
+    ak_dataset_details = dataset_details('AK', '/Users/jaapkruijt/Documents/ALANI/SPOT/SPOT_pilotadata/SPOT-mentions')
+    print('Experiment 1:')
+    print('utterances:')
+    print(en_dataset_details['utterances'])
+    print('mentions:')
+    print(en_dataset_details['mentions'])
+    print('turns:')
+    print(en_dataset_details['turns'])
+    print('median no transaction units:')
+    print(statistics.median(en_dataset_details['tu']))
+    print('repair:')
+    print(en_dataset_details['repair'])
+    print('dialog acts:')
+    print(en_dataset_details['da'].most_common())
+    print('median tu length:')
+    print(statistics.median(en_dataset_details['avg_length']))
+    print('Experiment 2:')
+    print('utterances:')
+    print(ak_dataset_details['utterances'])
+    print('mentions:')
+    print(ak_dataset_details['mentions'])
+    print('turns:')
+    print(ak_dataset_details['turns'])
+    print('median no transaction units:')
+    print(statistics.median(ak_dataset_details['tu']))
+    print('repair:')
+    print(ak_dataset_details['repair'])
+    print('dialog acts:')
+    print(ak_dataset_details['da'].most_common())
+    print('median tu length')
+    print(statistics.median(ak_dataset_details['avg_length']))
+
+
 if __name__ == "__main__":
-    # frame = combine_experiments()
+    frame = combine_experiments()
     # per_tu, per_round = organize_mentions()
-    # frame.to_pickle('data.pkl')
-    en_mean_sims = participant_linguistic_analysis('EN')
-    ak_mean_sims = participant_linguistic_analysis('AK')
-    print(ak_mean_sims)
-    en_mean_sims.to_pickle('en_sim.pkl')
-    ak_mean_sims.to_pickle('ak_sim.pkl')
+    frame.to_pickle('data.pkl')
+    en_mean_sims, en_mentions, en_round6 = participant_linguistic_analysis('EN')
+    ak_mean_sims, ak_mentions, ak_round6 = participant_linguistic_analysis('AK')
+    print('Experiment 1:')
+    for gameround, mentions in en_mentions['2'].items():
+        print(mentions)
+    print('Experiment 2:')
+    for gameround, mentions in ak_mentions['3'].items():
+        print(mentions)
+    # print(en_round6)
+    # print(ak_round6)
+    # print(ak_mean_sims)
+    # en_mean_sims.to_pickle('en_sim.pkl')
+    # ak_mean_sims.to_pickle('ak_sim.pkl')
     # for char, g_round in per_tu.items():
     #     print(f'CHARACTER: {char}')
     #     for r, unit in g_round.items():
@@ -231,4 +408,7 @@ if __name__ == "__main__":
     # print(counts)
     # print(data)
     # print(similarity)
+
+
+
 
--- visualize_data.py
+++ visualize_data.py
@@ -1,28 +1,51 @@
 import matplotlib.pyplot as plt
+from matplotlib import rcParams
+import matplotlib.image as mpimg
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 import seaborn as sns
 import pandas as pd
 
 if __name__ == "__main__":
     frame = pd.read_pickle('data.pkl')
     sns.set_theme()
+    # sns.set_theme(rc={'figure.figsize':(12,3)})
     sns.set_palette('colorblind')
-    sns.catplot(frame,  kind='bar', x='round', y='utt_len', hue='circle', col='experiment', col_order=['1', '2'])
-    # plt.show()
-    # plt.gcf()
-    plt.savefig('utt_len.png', bbox_inches='tight')
-    sns.catplot(frame, kind='bar', x='round', y='num_mentions', hue='circle', col='experiment', col_order=['1','2'])
-    plt.savefig('num_mentions.png', bbox_inches='tight')
+    g = sns.catplot(frame,  kind='bar', x='round', y='utt_len', hue='circle', col='experiment', col_order=['1','2'],
+                    height=4, aspect=1.3)
+    # plt.ylabel('Mean utterance length')
+    g.set_ylabels('Mean utterance length')
+    g.set_titles('Experiment {col_name}')
+    # rcParams['figure.figsize'] = 12,3
+    plt.savefig('utt_len.png', bbox_inches='tight', dpi=200)
+    h = sns.catplot(frame, kind='bar', x='round', y='num_mentions', hue='circle', col='experiment', col_order=['1','2'],
+                    height=4, aspect=1.3)
+    # plt.ylabel('Mean number of mentions')
+    h.set_ylabels('Mean number of mentions')
+    h.set_titles('Experiment {col_name}')
+    plt.savefig('num_mentions.png', bbox_inches='tight', dpi=200)
+    sns.catplot(frame, kind='bar', x='round', y='num_tus', hue='circle', col='experiment', col_order=['1','2'])
+    plt.show()
     sim_frame = frame[(frame['circle'] == 'inner') & ~(frame['round'] == '1')]
     sim_frame_en = frame[(frame['circle'] == 'inner') & ~(frame['round'] == '1') & (frame['experiment'] == '1')]
     sim_frame_ak = frame[(frame['circle'] == 'inner') & ~(frame['round'] == '1') & (frame['experiment'] == '2')]
-    # g = sns.FacetGrid(sim_frame, col='experiment', col_order=['1', '2'], hue='character')
-    # g.map(sns.catplot, 'round', 'sim_score')
     fig, (ax1,ax2) = plt.subplots(1, 2, sharey=True)
-    fig.set_figwidth(10)
+    ax1.set_title('Experiment 1')
+    ax2.set_title('Experiment 2')
+    ax1.set(ylabel='Cosine similarity score')
+    plt.subplots_adjust(wspace=0.1)
+    fig.set_figwidth(12)
+    fig.set_figheight(4)
     palette = sns.color_palette()
     sns.barplot(sim_frame_en, ax=ax1, x='round', y='sim_score', hue='character')
     sns.barplot(sim_frame_ak, ax=ax2, x='round', y='sim_score', hue='character', palette=palette[3:6])
-    plt.savefig('sim_score.png', bbox_inches='tight')
+    xtick_labels = ['1-2', '2-3', '3-4', '4-5', '5-6']
+    ax1.set_xticks([0,1,2,3,4])
+    ax2.set_xticks([0,1,2,3,4])
+    ax1.set_xticklabels(xtick_labels)
+    ax2.set_xticklabels(xtick_labels)
+    # ax1.get_legend().set_visible(False)
+    # ax2.get_legend().set_visible(False)
+    plt.savefig('sim_score.png', bbox_inches='tight', dpi=200)
     ak_sim = pd.read_pickle('ak_sim.pkl')
     en_sim = pd.read_pickle('en_sim.pkl')
     # print(ak_sim)
